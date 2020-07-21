2050b28a6665066ff741636796e75a9132400317
Nathan DeSpain <nathandespain@Nathans-MacBook-Pro.local>
2020-07-21 16:50:56 -0700

    oops false start now mvp

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,16 +1,48 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { axiosWithAuth } from '../utils/axiosWithAuth'
+
+const startFriendData =[
+  {
+    id: 1,
+    name: 'Joe',
+    age: 24,
+    email: 'joe@lambdaschool.com',
+  }
+]
 
 export default function Friends() {
-const [ friends, setFriends ] = useState([])
+  const [ friends, setFriends ] = useState(startFriendData)
+
+  const deleteFriend = id => {
+    axiosWithAuth()
+    .delete(`/api/friends/${id}`)
+    .then(response => {
+      console.log(response)
+      setFriends(friends.filter(friend => friend.id !== id))
+    })
+    .catch(error => console.log(error))
+  }
+
+  useEffect(() => {
+    axiosWithAuth()
+    .get('/api/friends')
+    .then(response => {
+      console.log("axios friends get response", response)
+      setFriends(response.data)
+    })
+  }, [])
+
   return (
     <>
       {friends.map(friend => {
+        const { name, age, email, id } = friend
         return(
-          <>
-            <h3>{friend.name}</h3>
+          <div key={id}>
+            <h3>{name}</h3>
             <p>{age}</p>
             <p>{email}</p>
-          </>
+            <button onClick={() => deleteFriend(id)}>Delete Friend</button>
+          </div>
         )
       })}
     </>
--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,17 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+
+const defaultCredentialsValue = {
+  username: "",
+  password: "",
+};
 
 export default function Login() {
+  const [ credentials, setCredentials ] = useState(defaultCredentialsValue);
+  const history = useHistory()
+
+  const handleChange = event => {
+    setCredentials({
+        ...credentials,
+        [event.target.name]: event.target.value
+    });
+    console.log(credentials)
+  };
+
+  const onLogin = event => {
+    event.preventDefault()
+    axios.post('http://localhost:5000/api/login', {
+      username: 'Lambda School', 
+      password: 'i<3Lambd4'
+    })
+    .then(response => {
+      localStorage.setItem("token", response.data.payload);
+      history.push("/protected/friends");
+    })
+    .catch(error => {
+      console.log(error)
+    });
+  }
+
   return (
     <>
-      <form>
+      <form onSubmit={onLogin}>
         <label>Username:
           <input 
             name="username"
+            value={credentials.username}
+            onChange={handleChange}
           />
         </label>
         <label>Password:
           <input 
             name="password"
+            type="password"
+            value={credentials.password}
+            onChange={handleChange}
           />
         </label>
         <button>Login</button>
